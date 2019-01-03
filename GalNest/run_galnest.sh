6896981419d35b3baf3bc687fe00c64ea161be46
amalyali_work <amalyali@mpe.mpg.de>
2019-01-03 18:29:29 +0000

    updated shell script

--- GalNest/run_galnest.sh
+++ GalNest/run_galnest.sh
@@ -10,8 +10,8 @@ module load astro/jun2017/multinest
 module load gpu/jun2017/montblanc
 
 export PYTHONPATH=/home/mrivi/.local/lib/python2.7/site-packages/montblanc-0.4.0_alpha3_324_ga3b51fd-py2.7.egg
-cd /share/splinter/alm/shape_measure
-export CUDA_VISIBLE_DEVICES=3  # (in order to select a smaller number of GPUs, by default montblanc uses all GPUs in the node)
+cd /share/splinter/alm/RadioGalFit/GalNest
+export CUDA_VISIBLE_DEVICES=3  # select a smaller number of GPUs (montblanc uses all GPUs in node by default)
 source activate boost_gcc5
 
 # MultiNest params
@@ -21,11 +21,10 @@ S_EFF=0.8
 EV_TOL=0.1
 N_LIVE=5000
 
-## Compute observed visibilities
+# Compute observed visibilities
 python compute_obs_vis.py /share/data1/alm/10_SKA1-1pol.ms/ -ns $NGAL > ./data/output_comp_vis_single-v4
 
-## Standard GalNest run
-mpiexec -n 1 python galnest-v4.py /share/data1/alm/10_SKA1-1pol.ms/ -ns 1 $NGAL $SEED $N_LIVE $S_EFF $EV_TOL > galnest_
-
+# Standard GalNest run
+mpiexec -n 1 python GalNest-v4.py /share/data1/alm/10_SKA1-1pol.ms/ -ns 1 $NGAL $SEED $N_LIVE $S_EFF $EV_TOL > galnest_
 
 source deactivate
