a9569e8ddb618829fc34baa973c9b034ea633bd2
amalyali_work <amalyali@mpe.mpg.de>
2019-03-06 14:06:58 +0100

    changed name of program computing SNR for each mode

---
+++ GalNest/compute_mode_SNR.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2019 Marzia Rivi
+#
+
+# Input: filename Measurement Set file
+#        SNR threshold
+#
+# To be run on the modal output from MultiNest.
+
+import sys
+import argparse
+import math
+import pyrap.tables as pt
+import numpy as np
+import montblanc
+import montblanc.util as mbu
+import pandas as pd
+import pickle
+from  montblanc.config import RimeSolverConfig as Options
+
+ARCS2RAD = np.pi/648000.
+l, m, s, a, e1_idx, e2_idx = 0, 1, 2, 3, 4, 5  # define parameter indexes
+
+parser = argparse.ArgumentParser(description='OBSERVED VISIBILITIES SIMULATION')
+parser.add_argument('msfile', help='Input MS filename')
+args = parser.parse_args(sys.argv[1:])
+
+seed = 1
+multinest_output_pkl = './data/low_snr/seed%s_14000_0.8_0.1_.pkl' % seed  # MultiNest modal output.
+multinest_output_pkl_w_snr = './data/low_snr/seed%s_14000_0.8_0.1_wSNR.pkl' % seed  # MultiNest modal output w SNR.
+
+# Set visibility noise variance (muJy)
+time_acc = 60
+efficiency = 0.9
+channel_bandwidth_hz = 240e6
+SEFD = 400e6
+sigma = (SEFD*SEFD)/(2.*time_acc*channel_bandwidth_hz*efficiency*efficiency)
+
+# Get the RIME solver
+slvr_cfg = montblanc.rime_solver_cfg(msfile=args.msfile,
+        sources=montblanc.sources(point=0, gaussian=0, sersic=1),
+        init_weights=None, weight_vector=False,
+        dtype='double', version='v4')
+
+with montblanc.rime_solver(slvr_cfg) as slvr:
+    # Set visibility noise variance (muJy)
+    slvr.set_sigma_sqrd(sigma)
+
+    # Initialise variables for Montblanc slvr.
+    nsrc, nssrc, ntime, nbl, nchan = slvr.dim_local_size('nsrc', 'nssrc', 'ntime', 'nbl', 'nchan')
+
+    lm = np.empty([1,2])
+    stokes = np.empty(shape=slvr.stokes.shape, dtype=slvr.stokes.dtype)
+    I = stokes[:,:,0]
+    alpha = slvr.ft(np.ones(nssrc*ntime)*(-0.7)).reshape(nsrc,ntime)
+    slvr.transfer_alpha(alpha)
+
+    # Read pickled MultiNest output.
+    data = pd.read_pickle(multinest_output_pkl)
+    df = pd.DataFrame.from_dict(data['modes'])
+    modes_SNR = []
+
+    for index, row in df.iterrows():
+        lm[:,0] = row['mean'][l]
+        lm[:,1] = row['mean'][m]
+        slvr.transfer_lm(lm)
+
+        flux = row['mean'][s]
+        I[:] = np.outer(flux, np.ones((ntime,)))
+        slvr.transfer_stokes(stokes)
+
+        e1 = row['mean'][e1_idx]
+        e2 = row['mean'][e2_idx]
+        R = row['mean'][a] * ARCS2RAD
+        sersic_shape = slvr.ft(np.array([e1, e2, R])).reshape((3,nssrc))
+        slvr.transfer_sersic_shape(sersic_shape)
+
+        # Create observed data and upload it to the GPU.
+        slvr.solve()
+        with slvr.context:
+            observed = slvr.retrieve_model_vis()
+
+        # Compute the SNR of the mode.
+        modes_SNR.append(np.sqrt(np.sum(np.square(np.absolute(observed))) / sigma))
+
+    # Add on column to dataframe from MultiNest. Write results to file
+    modes_SNR = np.asarray(modes_SNR)
+    df['SNR'] = modes_SNR
+    with open(multinest_output_pkl_w_snr, 'wb') as f:
+        pickle.dump(df, f)
